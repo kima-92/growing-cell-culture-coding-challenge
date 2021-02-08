3f6921df953f74c8d90f2fbc328e6e34a29e0c97
Wilma <kima_92@fastmail.com>
2021-02-07 22:36:33 -0500

    Testing coloring the cells based on state

--- Growing Cells in a Medium/Views/GridView.swift
+++ Growing Cells in a Medium/Views/GridView.swift
@@ -11,15 +11,17 @@ class GridView: UIView {
     
     // MARK: - Properties
     
-    var columns: Int?
-    var rows: Int?
+    var elements: [String]?
+    var columnsCount: Int?
+    var rowsCount: Int?
     var cellSize: CGFloat?
     
     // MARK: - Methods
     
     override func draw(_ rect: CGRect) {
-        guard let rows = rows,
-              let columns = columns,
+        guard let elements = elements,
+              let _ = rowsCount,
+              let _ = columnsCount,
               let cellSize = cellSize else { return }
         
         var x: CGFloat = 0
@@ -28,15 +30,19 @@ class GridView: UIView {
         if let context = UIGraphicsGetCurrentContext() {
             
             // Drawing a CGRect for each cell
-            for row in 0...rows {
-                for column in 0...columns {
+            for element in elements {
+                for character in element {
                     
                     let square = CGRect(x: x, y: y, width: cellSize, height: cellSize)
                     
-                    context.setFillColor(UIColor.red.cgColor)
-                    context.fill(square)
-                    context.setStrokeColor(UIColor.black.cgColor)
-                    context.stroke(square)
+                    // Color square based on state
+                    if character == "L" {
+                        context.setFillColor(UIColor.green.cgColor)
+                        context.fill(square)
+                    } else if character == "." {
+                        context.setFillColor(UIColor.red.cgColor)
+                        context.fill(square)
+                    }
                     
                     x += cellSize
                 }
@@ -51,8 +57,9 @@ class GridView: UIView {
     
     // Collect the data to size the grid
     func setGridSize(elements: [String], cellSize: CGFloat) {
-        self.rows = elements.count
-        self.columns = elements[0].count
+        self.elements = elements
+        self.rowsCount = elements.count
+        self.columnsCount = elements[0].count
         self.cellSize = cellSize
         
         // TODO: - Don't assume each row has the same count
